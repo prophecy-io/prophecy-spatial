476e6cdf3690b503c8f0a49bbeba23eb6ded40ce
akash deep <deep.akash@prophecy.io>
2026-04-06 17:02:50 +0530

    added spatial snowflake distance gem

--- gems/Distance.py
+++ gems/Distance.py
@@ -15,7 +15,7 @@ class Distance(MacroSpec):
     minNumOfInputPorts: int = 1
     supportedProviderTypes: list[ProviderTypeEnum] = [
         ProviderTypeEnum.Databricks,
-        # ProviderTypeEnum.Snowflake,
+        ProviderTypeEnum.Snowflake,
         # ProviderTypeEnum.BigQuery,
         # ProviderTypeEnum.ProphecyManaged
     ]

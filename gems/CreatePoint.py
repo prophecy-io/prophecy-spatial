eef897f67e987a412f242963b958de8786afac9f
akash deep <deep.akash@prophecy.io>
2026-04-14 16:16:51 +0530

    added spatial snowflake distance and createpoint gem

--- gems/CreatePoint.py
+++ gems/CreatePoint.py
@@ -16,7 +16,7 @@ class CreatePoint(MacroSpec):
     minNumOfInputPorts: int = 1
     supportedProviderTypes: list[ProviderTypeEnum] = [
         ProviderTypeEnum.Databricks,
-        # ProviderTypeEnum.Snowflake,
+        ProviderTypeEnum.Snowflake,
         # ProviderTypeEnum.BigQuery,
         # ProviderTypeEnum.ProphecyManaged
     ]

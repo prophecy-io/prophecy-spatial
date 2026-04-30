e23b7cb77c5276566eab89fc1b31589187d660b5
akash deep <deep.akash@prophecy.io>
2026-05-01 00:11:15 +0530

    added spatial snowflake buffer gem

--- gems/HeatMap.py
+++ gems/HeatMap.py
@@ -14,7 +14,7 @@ class HeatMap(MacroSpec):
     minNumOfInputPorts: int = 1
     supportedProviderTypes: list[ProviderTypeEnum] = [
         ProviderTypeEnum.Databricks,
-        # ProviderTypeEnum.Snowflake,
+        ProviderTypeEnum.Snowflake,
         # ProviderTypeEnum.BigQuery,
         # ProviderTypeEnum.ProphecyManaged
     ]
